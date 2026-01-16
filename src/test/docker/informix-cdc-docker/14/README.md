3af0d587ce0c1c9e2c7746ea833497d932242dcc
Jiri Pechanec <jiri.pechanec@centrum.cz>
2026-01-16 14:21:02 +0100

    debezium/dbz#1462 Remove unused Dockerfiles
    Signed-off-by: Jiri Pechanec <jiri.pechanec@centrum.cz>

--- src/test/docker/informix-cdc-docker/14/README.md
+++
@@ -1,5 +0,0 @@
-
-
-## References:
-
-- https://github.com/informix/informix-dockerhub-readme/blob/master/14.10.FC7W1/informix-developer-database.md#local-storage
--- src/test/docker/informix-cdc-docker/14/informix_post_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dbaccess sysadmin $BASEDIR/sql/informix_extend_root.sql >>$INIT_LOG 2>&1
-
-dbaccess sysadmin $BASEDIR/sql/informix_sbspace.sql >>$INIT_LOG 2>&1
-
-dbaccess sysadmin $INFORMIXDIR/etc/syscdcv1.sql >>$INIT_LOG 2>&1
-
-dbaccess sysadmin $INFORMIXDIR/etc/testdb.sql >>$INIT_LOG 2>&1
